cd06bf238778973f9db09fba9256c6f2df74f191
Midori-Gift <okometeberoyo5009@gmail.com>
2021-05-28 07:09:21 +0000

    book-edit-create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,12 +18,20 @@ class BooksController < ApplicationController
   end
 
   def show
+    @user = current_user
     @book = Book.find(params[:id])
     @books = Book.all
   end
 
   def edit
     @book = Book.find(params[:id])
+
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
 
   end
 
